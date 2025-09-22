04c45bb0065d16c1bbe5171e2613d054bfe77fd8
KrishLimbani <limbanikrish45@gmail.com>
2025-09-22 18:27:31 +0530

    demo

---
+++ client/src/components/ChatContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ChatContainer = () => {
+  return (
+    <div>ChatContainer</div>
+  )
+}
+
+export default ChatContainer
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Sidebar from '../components/Sidebar'
+import ChatContainer from '../components/ChatContainer'
+import RightSideBar from '../components/RightSideBar'
 
 const HomePage = () => {
+
+  const [SelectedUser, setSelectedUser] = useState(false);
   return (
-    <div>
-        <h1>Home Page</h1>
+    <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
+        <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${SelectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md: grid-cols-2'}` }>
+          <Sidebar SelectedUser={SelectedUser} setSelectedUser={setSelectedUser}/>
+          <ChatContainer SelectedUser={SelectedUser} setSelectedUser={setSelectedUser}/>
+          <RightSideBar SelectedUser={SelectedUser} setSelectedUser={setSelectedUser}/>
+        </div>
     </div>
   )
 }

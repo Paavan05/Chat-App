2db6f886deb667f59eef1468aef24b0e70b4b982
Paavan05 <paavanhn13@gmail.com>
2025-09-28 01:18:14 +0530

    chat app complete

--- server/server.js
+++ server/server.js
@@ -46,7 +46,12 @@ app.use("/api/messages", messageRouter);
 //Connect to MongoDB
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () =>
-  console.log(`Server is running on http://localhost:${PORT}`)
-);
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () =>
+    console.log(`Server is running on http://localhost:${PORT}`)
+  );
+}
+
+// Export server for Vercel
+export default server;

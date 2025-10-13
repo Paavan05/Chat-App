700d9d1ee91275ac3c846517cb0c398db28daf2c
Paavan05 <paavanhn13@gmail.com>
2025-10-13 12:07:02 +0530

    Backgroung Image Change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   const { authUser } = useContext(AuthContext);
 
   return (
-    <div className='bg-[url("/bgImage.svg")] bg-contain'>
+    <div className='bg-[url("/bgImage1.jpg")]'>
       <Toaster />
       <Routes>
         <Route path='/' element={<HomePage />} />

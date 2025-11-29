019fed9cc80b364629dc1fbf4f39e6d5e77dafc0
KrishLimbani <limbanikrish45@gmail.com>
2025-11-29 19:55:06 +0530

    ui fixed

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -10,17 +10,17 @@ const RightSidebar = () => {
   const [msgImages, setMsgImages] = useState([])
 
   // Get all images from the messages and set them to state
-  useEffect(()=>{
+  useEffect(() => {
     setMsgImages(messages.filter(msg => msg.image).map(msg => msg.image));
-  },[messages])
+  }, [messages])
 
   return selectedUser && (
-    <div className={` bg-[#8185B2]/10 w-full overflow-y-scroll text-white relative ${selectedUser ? "max-md:hidden" : ''}`}>
+    <div className={`relative bg-[#8185B3]/10 w-full h-full flex flex-col text-white  ${selectedUser ? "max-md:hidden" : ''}`}>
 
       {/* user image, name and bio */}
       <div className='pt-10 flex flex-col items-center gap-2 text-xs font-light mx-auto'>
         <div className='md:hidden absolute left-3 top-3'>
-          <img src={assets.arrow_icon} className='w-6 h-6 cursor-pointer'  />
+          <img src={assets.arrow_icon} className='w-6 h-6 cursor-pointer' />
         </div>
         <img src={selectedUser?.profilePic || assets.avatar_icon} alt=""
           className='w-20 aspect-[1/1] rounded-full' />
@@ -34,9 +34,9 @@ const RightSidebar = () => {
 
       <hr className='border-[#ffffff50] my-4' />
 
-      <div className='px-5 text-xs'>
-        <p>Media</p>
-        <div className='mt-2 max-h-[200px] overflow-y-scroll grid grid-cols-2
+      <div className='px-5 text-xs flex-1 overflow-y-auto '>
+        <p className='text-sm'>Media</p>
+        <div className='mt-2 max-h-[335px] overflow-y-scroll grid grid-cols-2
         gap-4 opacity-80'>
           {msgImages.map((url, index) => (
             <div key={index} onClick={() => window.open(url)} className='cursor-pointer rounded'>
@@ -46,7 +46,7 @@ const RightSidebar = () => {
         </div>
       </div>
 
-      <div className='sticky bottom-0 left-0 w-full px-5 pb-6 pt-4 bg-gradient-to-t from-[#0f0f12]/80 via-[#0f0f12]/50 to-transparent'>
+      <div className=' bottom-0 left-0 w-full px-5 pb-6 pt-4 bg-gradient-to-t from-[#0f0f12]/80 via-[#0f0f12]/50 to-transparent'>
         <div className='flex gap-3 max-md:flex-col'>
           <button
             type='button'
@@ -58,9 +58,9 @@ const RightSidebar = () => {
                 console.error(e);
               }
             }}
-            className='flex-1 bg-gradient-to-r from-[#FF4D4D] to-[#B30000] text-white text-xs font-medium py-3 rounded-full cursor-pointer transition hover:opacity-90'
+            className='flex-1 bg-gradient-to-r from-[#FF4D4D] to-[#B30000] text-white text-sm font-medium py-3 rounded-full cursor-pointer transition hover:opacity-90'
           >
-            Remove Friend
+            Remove friend
           </button>
           <button
             type='button'
